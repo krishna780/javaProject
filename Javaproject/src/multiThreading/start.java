9d0af2338024404b7d42d12b631017e577614d29
krishna kanth gona <krishna05411@gmail.com>
2019-11-15 10:12:26 -0500

    start

--- Javaproject/src/multiThreading/start.java
+++ Javaproject/src/multiThreading/start.java
@@ -1,10 +1,14 @@
 package multiThreading;
 
-public class start {
+public class start extends Thread {
+	public void run() {
+        System.out.println("running a thread");
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("start method");
+		start t1 = new start();
+		t1.start();
 	}
 
 }
